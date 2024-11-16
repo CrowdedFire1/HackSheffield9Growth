ba0fea485fe53d104a87393517f5bf655d6d9176
Joshua Rylance <joshua@rylance.biz>
2024-11-16 17:10:08 +0000

    got replciate api key

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.write("testing")
+import os
+
+#I know this is bad, but its the way to get this to work and share it, pls dont sue me
+os.environ["REPLICATE_API_TOKEN"] = "r8_Opb9zz5neiFK6q8qVlxxNOY7AnowT150QYKgE"
