42dd6e5d962962b5b768df9187ee1014ea061276
Josh <159453968+CrowdedFire1@users.noreply.github.com>
2024-11-17 10:37:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,3 @@ import streamlit as st
 
 import os
 
-#I know this is bad, but its the way to get this to work and share it, pls dont sue me
-os.environ["REPLICATE_API_TOKEN"] = "r8_Opb9zz5neiFK6q8qVlxxNOY7AnowT150QYKgE"
